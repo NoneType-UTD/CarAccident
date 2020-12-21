afdc8b5d27450a46c73aafd633a301e5c0f7353d
Fawaz Ahmed <fawazahmed3@outlook.com>
2020-12-20 22:58:05 -0600

    Differences between the two output set, format is in {word : [corenlp result, nltk result]}

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import nltk
 import en_core_web_sm
 import requests
 import json
 from jsondiff import diff
 import string
+import KeyWordExtraction
+import nltk
+from pathlib import Path
 
 nlp = en_core_web_sm.load()
 
@@ -14,68 +16,97 @@ def preprocess(sent):
     return sent
 
 
+def parseSentence(data):
+    data = nltk.word_tokenize(data)
+    data = nltk.pos_tag(data)
+    cp = nltk.RegexpParser('NP: {<DT>?<JJ>*<NN>}')
+    return cp.parse(data)
+
+
+def simplifyNLTKResult(data):
+    result2 = []
+    for x in data:
+        if type(x) is tuple:
+            result2.append(x)
+        else:
+            for y in x:
+                result2.append(y)
+    return [idx for idx in result2 if not any(punc in idx for punc in string.punctuation)]
+
+
+def simplifyStandfordResult(data):
+    pre_tuple_form = []
+    result3 = []
+    for x in range(len(data['parse_sentence'])):
+        if data['parse_sentence'][x] == '(':
+            newWord = ''
+            is_in_parentheses = True
+        else:
+            is_in_parentheses = False
+        while is_in_parentheses:
+            newWord += data['parse_sentence'][x]
+            x += 1
+            if data['parse_sentence'][x] == ')':
+                is_in_parentheses = False
+                pre_tuple_form.append((newWord + ')').replace(' ', ','))
+            elif data['parse_sentence'][x] == '(':
+                is_in_parentheses = False
+    for z in pre_tuple_form:
+        word_and_POS = z.replace('(', '').replace(')', '').split(',')
+        finalTuple = (word_and_POS[1], word_and_POS[0])
+        result3.append(finalTuple)
+    return [idx for idx in result3 if not any(punc in idx for punc in string.punctuation)]
+
+
 def Diff(li1, li2):
     li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2]
     return li_dif
 
 
+def CompareSentences(sentenceData):
+    postProcess = preprocess(sentenceData['sentence'])
+    pattern = 'NP: {<DT>?<JJ>*<NN>}'
+    cp = nltk.RegexpParser(pattern)
+    result = cp.parse(postProcess)
+    nltkResult = dict(simplifyNLTKResult(result))
+    stanfordResult = dict(simplifyStandfordResult(sentenceData))
+    resultDiff = [{k: v} for k, v in dict(diff(nltkResult, stanfordResult, syntax='symmetric')).items()]
+    resultDiff = resultDiff[: len(resultDiff) - 2]
+    return resultDiff
+
+
+def SaveToFile(file, data):
+    my_file = Path(file)
+    if my_file.is_file():
+        # This works so I won't touch it.
+        with open(file, 'r') as f1:
+            oldData = json.load(f1)
+        with open(file, 'w') as f1:
+            newData = oldData + data
+            json.dump(newData, f1, indent=4)
+    else:
+        with open(file, 'w') as f1:
+            json.dump(data, f1, indent=4)
+
+
 if __name__ == '__main__':
+    tr4w = KeyWordExtraction.TextRank4Keyword()
+
     with open('ArticleIds.json', 'r') as f:
         article_ids = json.load(f)
     for _id in article_ids:
-        response = requests.get('http://eventdata.utdallas.edu/api/article?doc_id=' + _id['_id'])
-        data = response.json()['data']
-        nltkResult = []
-        stanfordResult = []
+        response = requests.get('http://149.165.156.117/api/article?doc_id=' + _id['_id'])
+        articleData = response.json()['data']
 
-        for sentence in data:
-            pre_tuple_form = []
-            sentence2 = preprocess(sentence['sentence'])
-            pattern = 'NP: {<DT>?<JJ>*<NN>}'
-            cp = nltk.RegexpParser(pattern)
-            result = cp.parse(sentence2)
+        for rawSentence in articleData:
+            fileName = f"{_id['_id']}.json"
+            '''
+            Uncomment to find the differences of the standford corenlp and NLTK package on the sentences
+            nlpDiff = CompareSentences(rawSentence)
+            SaveToFile(fileName, nlpDiff)
+            '''
 
-            for x in result:
-                if type(x) is tuple:
-                    stanfordResult.append(x)
-                else:
-                    for y in x:
-                        stanfordResult.append(y)
+            tr4w.analyze(rawSentence['sentence'], candidate_pos=['NOUN', 'PROPN'], window_size=4, lower=False)
+            test = [{k: v} for k, v in dict(tr4w.get_keywords(10)).items()]
+            SaveToFile(fileName, test)
 
-            # The data from the stanford corenlp is in string so I have to convert it into a tuple.
-            for x in range(len(sentence['parse_sentence'])):
-                if sentence['parse_sentence'][x] == '(':
-                    newWord = ''
-                    is_in_parentheses = True
-                else:
-                    is_in_parentheses = False
-                while is_in_parentheses:
-                    newWord += sentence['parse_sentence'][x]
-                    x += 1
-                    if sentence['parse_sentence'][x] == ')':
-                        is_in_parentheses = False
-                        pre_tuple_form.append((newWord + ')').replace(' ', ','))
-                    elif sentence['parse_sentence'][x] == '(':
-                        is_in_parentheses = False
-            for z in pre_tuple_form:
-                word_and_POS = z.replace('(', '').replace(')', '').split(',')
-                finalTuple = (word_and_POS[1], word_and_POS[0])
-                nltkResult.append(finalTuple)
-
-            stanfordResult = [idx for idx in stanfordResult if not any(punc in idx for punc in string.punctuation)]
-            nltkResult = [idx for idx in nltkResult if not any(punc in idx for punc in string.punctuation)]
-            with open(f"{_id['_id']}.json", 'w') as f1:
-                newFile = False
-                try:
-                    oldData = json.load(f1)
-                except:
-                    newFile = True
-                parsed1 = dict(stanfordResult)
-                parsed2 = dict(nltkResult)
-                nlpDiff = [{k: v} for k, v in dict(diff(parsed1, parsed2, syntax='symmetric')).items()]
-                nlpDiff = nlpDiff[: len(nlpDiff) - 2]
-                if newFile:
-                    json.dump(nlpDiff, f1, indent=4)
-                else:
-                    oldData.update(nlpDiff)
-                    json.dump(oldData, f1, indent=4)
